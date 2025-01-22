6f00963757d8df2e9af2f3372329c8f6ca4a46c3
ahtwet12 <anhtuyetdu21@gmail.com>
2025-01-22 21:05:26 +0700

    axiot_call_api

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -1,16 +1,28 @@
+import { useEffect, useState } from 'react'
 import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from '~/pages/Boards/BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import {mockData} from '~/apis/mock-data'
+import { fetchBoardDetailsAPI } from '~/apis'
+
+
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId = '6790e16d260f90d233498e06' //dang fix cung
+    //Call API
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
   return (
     <>
       <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-
         <AppBar/>
-        <BoardBar board={mockData?.board} />
-        <BoardContent board={mockData?.board}/>
+        <BoardBar board={board} />
+        <BoardContent board={board}/>
 
       </Container>
     </>
